aa595e19d4910917f1c9c369418737e5aed0be11
TOVTC <veronica.tc.to@outlook.com>
2023-07-05 10:22:46 -0400

    scope

--- PI.ps1
+++ PI.ps1
@@ -4,7 +4,7 @@ Write-Host "The value of `$PI is $PI"
 
 # Run the script by invoking its name and path: ./PI.ps1
 
-# Variables
+# ---Variables---
 $PI = 3.14
 
 # Single quotation marks specify literals, double quotation marks allow variables to be interpolated
@@ -16,3 +16,13 @@ Write-Host "Here is `$PI and its value is $PI"
 
 # Expressions can also be written within double quotations
 Write-Host "An expression $($PI + 1)"
+
+# ---Scope---
+# Scope can be Global (to the system), Script (within a script file), or Local (current scope)
+# A script can target global scope by prepending the global keyword
+# Scopes can nest and can have parent and child scopes - items are visible in the current and child scopes
+
+# A variable defined directly in the console is global scope
+# Running this script file after entering "$test = 'hi'" in the console will allow $test to print
+
+$test
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # powershell-intro-tutorial
-*   Powershell protects against unintentional actions by requiring full or relative paths when running scripts and through execution policy
