672b2b01ddd5e299bdfa1a945d6a09a9587b4991
TOVTC <veronica.tc.to@outlook.com>
2023-07-05 15:01:41 -0400

    error handling/flow control exercise

--- Backup.ps1
+++ Backup.ps1
@@ -5,6 +5,19 @@ Param(
   [string]$Path = './app',
   [string]$DestinationPath = './'
 )
+# Checks to see if provided path exists
+If (-Not (Test-Path $Path))
+{
+    Throw "The source directory $Path does nto exist, please specify an existing directory"
+}
 $date = Get-Date -format "yyyy-MM-dd"
-Compress-Archive -Path $Path -CompressionLevel 'Fastest' -DestinationPath "$($DestinationPath + 'backup-' + $date)"
-Write-Host "Created backup at $($DestinationPath + 'backup-' + $date + '.zip')"
+# Create variable to check if file already exists
+$DestinationFile = "$($DestinationPath + 'backup-' + $date + '.zip')"
+# Check if file already exists
+If (-Not (Test-Path $DestinationFile))
+{
+    Compress-Archive -Path $Path -CompressionLevel 'Fastest' -DestinationPath "$($DestinationPath + 'backup-' + $date)"
+    Write-Host "Created backup at $($DestinationPath + 'backup-' + $date + '.zip')"
+} Else {
+    Write-Error "Today's backup already exists"
+}
