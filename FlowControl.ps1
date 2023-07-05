8d439e7db1e70ad4656dd56e3f205a661834fb96
TOVTC <veronica.tc.to@outlook.com>
2023-07-05 14:39:25 -0400

    control flow

---
+++ FlowControl.ps1
@@ -0,0 +1,24 @@
+﻿# ---Flow Control---
+# Sanitize input - ensure parameters being accepted hold reasonable values
+# Control execution flow - determines which group of statements runs
+# Iterate over data - execute code for each item in a list
+
+# ---If/Else If/Else---
+$Value = 3
+# -le less than or equal to
+If ($Value -le 0)
+{
+    Write-Host "Is negative"
+} Else {
+    Write-Host "Is positive"
+}
+
+$Status = 'Minor'
+If ($Status -eq 'Minor')
+{
+    Write-Host $False
+} ElseIf ($Status -eq 'Adult') {
+    Write-Host $True
+} Else {
+    Write-Host $False
+}
