b4dfdb40464ffc64cde6e606501767e69ef45e5e
TOVTC <veronica.tc.to@outlook.com>
2023-07-05 09:46:27 -0400

    variables

---
+++ PI.ps1
@@ -0,0 +1,18 @@
+﻿# PI.ps1
+$PI = 3.14
+Write-Host "The value of `$PI is $PI"
+
+# Run the script by invoking its name and path: ./PI.ps1
+
+# Variables
+$PI = 3.14
+
+# Single quotation marks specify literals, double quotation marks allow variables to be interpolated
+Write-Host 'Here is $PI'
+Write-Host "Here is $PI"
+
+# A backtick escapes the first instance of $PI
+Write-Host "Here is `$PI and its value is $PI"
+
+# Expressions can also be written within double quotations
+Write-Host "An expression $($PI + 1)"
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # powershell-intro-tutorial
+*   Powershell protects against unintentional actions by requiring full or relative paths when running scripts and through execution policy
